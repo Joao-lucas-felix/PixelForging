53648560a251ad7dc7d572c01a53f7ea42220f36
Joao-lucas-felix <jlmacena06@gmail.com>
2024-12-21 14:33:32 -0300

    Refactor: changing the implementation of ListingPixels to use a Worker pools pattern

--- src/image-processing/pixel-forging.go
+++ src/image-processing/pixel-forging.go
@@ -7,53 +7,56 @@ import (
 	_ "image/gif"  // Para suporte a GIF
 	_ "image/jpeg" // Para suporte a JPEG
 	"image/png"
-	_ "image/png" // Para suporte a PNG
 	"log"
 	"os"
 	"sync"
 )
 
-// ListingPixels this function lists all the pixels in the image, this function does not preserve the order of the pixels
-// if in the image the order of the pixels is X - Y in the final list of pixels it is possible that the pixels are shuffled and in the result the order may be Y - X
-//
-//	Use ListingPixelsOrded to get the list in the order of image
+type lineProcessingUtil struct {
+	img    image.Image
+	bounds image.Rectangle
+}
+
+// ListingPixels lists all the pixels in an image as a slice of `color.RGBA`.
+// This function uses a worker pool to process the image in parallel, so the order
+// of pixels in the result may not correspond to the original (row-column) order in the image.
+// Use ListingPixelsOrdered if you need the pixel order to match the image's structure.
 func ListingPixels(filePath string) ([]color.RGBA, error) {
-	file, err := os.Open(filePath)
+
+	img, err := decodeImage(filePath)
 	if err != nil {
-		fmt.Println("Erro ao abrir a imagem:", err)
+		fmt.Println("Error while trying to open the image", err)
 		return nil, err
 	}
-	defer file.Close()
-
-	img, _, err := image.Decode(file)
-	if err != nil {
-		fmt.Println("Erro ao decodificar a imagem:", err)
-		return nil, err
-	}
-
-	bounds := img.Bounds()
 	var wg sync.WaitGroup
-	colorsChan := make(chan color.RGBA, bounds.Dx()*10)
-	sema := make(chan struct{}, 10)
-	var colors []color.RGBA
+	bounds := img.Bounds()
+	colorsChan := make(chan color.RGBA)
+	colors := make([]color.RGBA, 0, bounds.Dx()*bounds.Dy())
+
+	pools := 0
+	numbersOfLines := bounds.Dy()
+
+	if numbersOfLines < 32 {
+		pools = numbersOfLines
+	} else {
+		pools = 32
+	}
+
+	linesToProcess := make(chan int, pools)
+
+	for i := 0; i < pools; i++ {
+		wg.Add(1)
+		go woker(linesToProcess, colorsChan, lineProcessingUtil{
+			img:    img,
+			bounds: bounds,
+		}, &wg)
+	}
 
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		wg.Add(1)
-		sema <- struct{}{}
-		go func(y int) {
-			defer wg.Done()
-			defer func() {
-				<-sema
-			}()
-
-			for x := bounds.Min.X; x < bounds.Max.X; x++ {
-				r, g, b, a := img.At(x, y).RGBA()
-				colorsChan <- color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: uint8(a)}
-			}
-
-		}(y)
-
+		linesToProcess <- y
 	}
+	close(linesToProcess)
+
 	go func() {
 		wg.Wait()
 		close(colorsChan)
@@ -65,18 +68,28 @@ func ListingPixels(filePath string) ([]color.RGBA, error) {
 	return colors, nil
 }
 
-// ListingPixelsOrded this function iterates through an image and returns all the pixels in a slice of color.RGBA, this function preserves the order in which the pixels appear in the image
-func ListingPixelOrded(filePath string) ([]color.RGBA, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Erro ao abrir a imagem:", err)
-		return nil, err
+func woker(linesChan chan int, colorsChan chan color.RGBA, util lineProcessingUtil, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for y := range linesChan {
+		getPixelsOfImageLine(util.img, y, util.bounds, colorsChan)
 	}
-	defer file.Close()
 
-	img, _, err := image.Decode(file)
+}
+
+func getPixelsOfImageLine(img image.Image, line int, bounds image.Rectangle, colorsChan chan color.RGBA) {
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		r, g, b, a := img.At(x, line).RGBA()
+		colorsChan <- color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: uint8(a)}
+	}
+}
+
+// ListingPixelsOrdered iterates through an image and returns all its pixels as a slice of `color.RGBA`.
+// The function preserves the original order of the pixels in the image (row by row).
+func ListingPixelsOrdered(filePath string) ([]color.RGBA, error) {
+
+	img, err := decodeImage(filePath)
 	if err != nil {
-		fmt.Println("Erro ao decodificar a imagem:", err)
+		fmt.Println("Error while trying to open the image", err)
 		return nil, err
 	}
 
@@ -94,35 +107,47 @@ func ListingPixelOrded(filePath string) ([]color.RGBA, error) {
 	return colors, nil
 }
 
+// ExtractColorPalette extracts the color palette of an image and prints the RGBA values of unique colors.
+// Parameters:
+// - inputFilePath: the path to the input image file.
+// - outPutFilePath: the path to the output file (currently unused).
 func ExtractColorPalette(inputFilePath, outPutFilePath string) {
 	colors, err := ListingPixels(inputFilePath)
 	if err != nil {
 		log.Fatalln("Error while trying to read the image pixels: ", err)
 	}
-	var uniqueColors []color.RGBA
-	for _, color := range colors {
-		addColorIfNotExists(&uniqueColors, color)
-	}
+
+	uniqueColors := getUniqueColors(colors)
 	for _, color := range uniqueColors {
 		fmt.Println(color.R, color.G, color.B, color.A)
 	}
 }
 
-func addColorIfNotExists(uniqueColors *[]color.RGBA, color color.RGBA) {
-	if len(*uniqueColors) == 0 {
-		*uniqueColors = append(*uniqueColors, color)
-		return
-	} else {
-		colorExists := false
-		for _, v := range *uniqueColors {
-			colorExists = v == color
-		}
-		if colorExists {
-			return
-		}
-		*uniqueColors = append(*uniqueColors, color)
-		return
+// addColorIfNotExists adds a color to the slice of unique colors if it does not already exist.
+func getUniqueColors(colors []color.RGBA) []color.RGBA {
+	uniqueColors := make(map[color.RGBA]struct{})
+	for _, color := range colors {
+		uniqueColors[color] = struct{}{}
 	}
+	result := make([]color.RGBA, 0, len(uniqueColors))
+
+	for key := range uniqueColors {
+		result = append(result, key)
+	}
+	return result
+}
+
+func decodeImage(filePath string) (image.Image, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao abrir a imagem: %w", err)
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao decodificar a imagem: %w", err)
+	}
+	return img, nil
 }
 
 // CreateImage3x3 is a temp fuction for dev tests
