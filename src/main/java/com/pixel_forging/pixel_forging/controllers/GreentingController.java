5888d5479a1b775e67e78d13af192c4e6440bacf
Joao-lucas-felix <jlmacena06@gmail.com>
2025-12-13 14:51:34 -0300

    Feature/testing auto deploy change

--- src/main/java/com/pixel_forging/pixel_forging/controllers/GreentingController.java
+++ src/main/java/com/pixel_forging/pixel_forging/controllers/GreentingController.java
@@ -1,6 +1,8 @@
 package com.pixel_forging.pixel_forging.controllers;
 
 import com.pixel_forging.pixel_forging.dtos.StatusDto;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/wake")
 public class GreentingController {
+    private static final Log log = LogFactory.getLog(GreentingController.class);
+
     @GetMapping
     public ResponseEntity<StatusDto> wake() {
+        log.info("The server is WakeUp!");
         return  ResponseEntity.ok(new StatusDto("UP!"));
     }
 }
